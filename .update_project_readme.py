0cfb57a1760067fd005e8e2381047aa9794199d2
Carl Simon Adorf <csadorf@umich.edu>
2017-08-02 13:58:02 -0400

    Fix links created by update_project_readme file.
    And fix the script as well.

--- .update_project_readme.py
+++ .update_project_readme.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 import os
-import sys
 import glob
 from mistune import BlockLexer
 
@@ -18,7 +17,7 @@ def generate_project_overview(file):
     for fn in sorted(glob.glob('projects/*/README.md'), key=lambda fn: (fn.count('.'), fn)):
         dirname = os.path.dirname(fn)
         project_name = os.path.basename(dirname)
-        file.write("### [{}]({}/)\n\n".format(project_name, dirname))
+        file.write("### [{}]({}/)\n\n".format(project_name, os.path.basename(dirname)))
         with open(fn) as readme:
             files = BlockLexer().parse(readme.read())
             file.write(_find_descr(files) + '\n')
@@ -53,7 +52,14 @@ def generate_readme(readme, tmp):
 if __name__ == '__main__':
     fn_readme = 'projects/README.md'
     fn_readme_tmp = fn_readme + '.tmp'
-    with open(fn_readme) as readme:
-        with open(fn_readme_tmp, 'x') as tmp:
-            generate_readme(readme, tmp)
-    os.rename(fn_readme_tmp, fn_readme)
+    try:
+        with open(fn_readme) as readme:
+            with open(fn_readme_tmp, 'x') as tmp:
+                generate_readme(readme, tmp)
+    except Exception:
+        try:
+            os.remove(fn_readme_tmp)
+        except OSError:
+            pass
+    else:
+        os.rename(fn_readme_tmp, fn_readme)
