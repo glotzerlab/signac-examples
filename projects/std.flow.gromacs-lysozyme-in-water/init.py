6378d904971d4bb7c55c121b1c03d384c6a4b0d4
Carl Simon Adorf <csadorf@umich.edu>
2018-01-18 17:41:15 -0500

    Do not add mdp files to repo, but download during init.

--- projects/std.flow.gromacs-lysozyme-in-water/init.py
+++ projects/std.flow.gromacs-lysozyme-in-water/init.py
@@ -1,12 +1,23 @@
 #!/usr/bin/env python
 """Initialize the project's data space."""
-import signac
+import os
 import subprocess
 
-PDB_URL = "https://files.rcsb.org/download/"
+import signac
 
+PDB_URL = "https://files.rcsb.org/download/"
+MDP_URL = "http://www.bevanlab.biochem.vt.edu/Pages/Personal/justin/gmx-tutorials/lysozyme/Files/"
+
+
+# Initialize signac project
 project = signac.init_project('std.flow.gromacs-example-project')
 
+# Download MDP files from tutorial website
+os.makedirs('mdp_files', exist_ok=True)
+for fn in ('em.mdp', 'ions.mdp', 'md.mdp', 'npt.mdp', 'nvt.mdp'):
+    subprocess.check_output(["curl", URL + fn, "-o", project.fn("mdp_files/" + fn)])
+
+# Initialize data space for one protein
 for protein in ["1AKI"]:
     job = project.open_job(dict(protein=protein))
     with job:
