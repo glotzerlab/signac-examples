3aa9e8f701e6bfca68b343de4f806342c44cba92
CryoZEUS <yashbhardwaj911@gmail.com>
2020-03-16 19:16:30 +0530

    trying old run-tests.sh

---
+++ projects/run-tests.sh
@@ -0,0 +1,15 @@
+ 
+#!/bin/bash
+
+set -e
+
+for project in `ls -d */`; do
+    requirements=$project/requirements.txt
+    echo "Run test for $project."
+    if [ -e $requirements ]
+    then
+        cat $requirements
+        conda install --yes --use-index-cache --file $requirements
+    fi
+    flow-test $project -vv --timeout=600 $@
+done
