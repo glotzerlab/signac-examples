aea90f480db88a3db57caa139249c02ae4c6a486
Bradley Dice <bdice@bradleydice.com>
2021-02-04 19:11:00 -0600

    Use --pretend instead of --test.

--- projects/flow.hoomd.lj/test.sh
+++ projects/flow.hoomd.lj/test.sh
@@ -1,6 +1,6 @@
 python src/init.py 0
 python src/project.py status -d -p 1
-python src/project.py submit --test
+python src/project.py submit --pretend
 python src/project.py run -o initialize --progress
 python src/project.py run -o estimate --progress
 python src/project.py run -o sample --progress
