d59059218c8455b311807cdff68d92cd49c8f2a3
Bradley Dice <bdice@bradleydice.com>
2021-02-09 23:00:53 -0600

    Update notebook rendering script.

--- .render_all_notebooks.sh
+++ .render_all_notebooks.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
+
+# Remove existing signac workspaces used by notebooks
+rm -r notebooks/projects
+
+# Render all notebooks by executing them and overwriting contents
 jupyter nbconvert --execute --inplace notebooks/*.ipynb
+
+# Clean up the notebooks using pre-commit hooks (formatters)
+# Importantly, this removes extra metadata and strips user paths
+pre-commit run --files notebooks/*.ipynb
