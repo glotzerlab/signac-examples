5f2cbd5e946e46d549fec0de37957c2bc7d0cc9c
Bradley Dice <bdice@bradleydice.com>
2021-05-17 09:59:03 -0500

    Apply suggestions from code review
    Co-authored-by: Joshua A. Anderson <joaander@umich.edu>

--- projects/flow.aggregation-mpi/project.py
+++ projects/flow.aggregation-mpi/project.py
@@ -15,11 +15,12 @@ JOBS_PER_AGGREGATE = 8
 def do_mpi_task(*jobs):
     from mpi4py import MPI
 
-    comm = MPI.COMM_WORLD
-    rank = comm.Get_rank()
-    size = comm.Get_size()
+    world_comm = MPI.COMM_WORLD
+    world_rank = comm.Get_rank()
+    world_size = comm.Get_size()
     num_jobs = len(jobs)
-    print(f"Launching MPI tasks for {len(jobs)} jobs. Rank {rank} of {size}.")
+    if world_rank == 0:
+        print(f"Launching MPI tasks for {len(jobs)} jobs. Rank {rank} of {size}.")
 
     # Use MPI splitting to make new communicators
     communicator_size = size // num_jobs
@@ -30,6 +31,8 @@ def do_mpi_task(*jobs):
     split_comm = comm.Split(color, key)
     split_rank = split_comm.Get_rank()
     split_size = split_comm.Get_size()
+
+    # Select the job from the aggregate to run in the split communicator.
     job = jobs[color]
 
     print(f"{rank=}, {split_rank=}, {split_size=}, {job=}")
