f1b805461143f10b476d71b1aa9d944a9de7226a
Bradley Dice <bdice@bradleydice.com>
2021-05-17 10:00:49 -0500

    Rename world variables.

--- projects/flow.aggregation-mpi/project.py
+++ projects/flow.aggregation-mpi/project.py
@@ -20,22 +20,22 @@ def do_mpi_task(*jobs):
     world_size = comm.Get_size()
     num_jobs = len(jobs)
     if world_rank == 0:
-        print(f"Launching MPI tasks for {len(jobs)} jobs. Rank {rank} of {size}.")
+        print(f"Launching MPI tasks for {len(jobs)} jobs. Rank {world_rank} of {world_size}.")
 
     # Use MPI splitting to make new communicators
-    communicator_size = size // num_jobs
-    color = rank // communicator_size
-    key = rank % communicator_size
-    print(f"{rank=}, {color=}, {key=}")
+    communicator_size = world_size // num_jobs
+    color = world_rank // communicator_size
+    key = world_rank % communicator_size
+    print(f"{world_rank=}, {color=}, {key=}")
 
-    split_comm = comm.Split(color, key)
+    split_comm = world_comm.Split(color, key)
     split_rank = split_comm.Get_rank()
     split_size = split_comm.Get_size()
 
     # Select the job from the aggregate to run in the split communicator.
     job = jobs[color]
 
-    print(f"{rank=}, {split_rank=}, {split_size=}, {job=}")
+    print(f"{world_rank=}, {split_rank=}, {split_size=}, {job=}")
 
     # Now you can use the split communicator with your application!
     # Call your function with job, split_comm
