08348224509d4755d8e114640e538d7f730ebe8f
Brandon Butler <butlerbr@umich.edu>
2021-05-20 13:41:31 -0400

    2D-RW simplify init variable specification
    Co-authored-by: Bradley Dice <bdice@bradleydice.com>

--- projects/flow.groups-2D-random-walk/src/init.py
+++ projects/flow.groups-2D-random-walk/src/init.py
@@ -12,17 +12,16 @@ logger = logging.getLogger()
 # Lower level to prevent output
 logger.setLevel(logging.WARN)
 
-N_STD = 20
 # The different standard deviations to use for generating moves in the random
 # walk
-STANDARD_DEVIATIONS = np.linspace(0.1, 1, N_STD)
+STANDARD_DEVIATIONS = np.linspace(start=0.1, stop=1, num=20)
 NUMBER_REPLICAS = 100
-RUN_N_STEPS = 5_000
+RUN_STEPS = 5_000
 
 
 def main():
     project = signac.init_project("2D Gaussian Random Walk")
-    project.doc.run_steps = RUN_N_STEPS
+    project.doc.run_steps = RUN_STEPS
     for replica in range(NUMBER_REPLICAS):
         for std in STANDARD_DEVIATIONS:
             statepoint = {"mean": 0, "std": std, "replica": replica + 1}
