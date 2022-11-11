5ae22fdf798aa54e4648647310f2883960e006a5
Corwin Kerr <cbkerr@umich.edu>
2022-11-11 13:06:09 -0500

    Add Notes and image labels to dashboard

--- projects/flow.pytorch.HyperparameterOptmize/dashboard.py
+++ projects/flow.pytorch.HyperparameterOptmize/dashboard.py
@@ -4,14 +4,21 @@ from signac_dashboard.modules import (
     StatepointList,
     VideoViewer,
     DocumentList,
+    Notes
 )
 
 class MyDashboard(Dashboard):
     def job_title(self, job):
         return f"Total epochs={job.sp.epochs}, Learning rate={job.sp.lr}, Latent space dimension={job.sp.latent_dim}"
 
+    def job_sorter(self, job):
+        return (-job.sp.lr, job.sp.hidden_dim, job.sp.epochs)
 
 if __name__ == "__main__":
     MyDashboard(
-        modules=[ImageViewer(), StatepointList(), VideoViewer(), DocumentList()]
+        modules=[ImageViewer(img_globs=["Loss.jpg"],name="Learning Curve"),
+                 ImageViewer(img_globs=["latent.jpg"],name="Latent Space"),
+                 ImageViewer(img_globs=["digits_recon.jpg"], name="Reconstructed"),
+                 ImageViewer(img_globs=["digits_orig.jpg"],name="Original"),
+                 StatepointList(), VideoViewer(), DocumentList(), Notes()]
     ).main()
