b618da03745ac8ce13a27ef64deab1e9f5e6cc34
Bradley Dice <bdice@bradleydice.com>
2023-04-19 13:21:17 -0500

    Update idg-minimal.

--- projects/idg-minimal/idg.py
+++ projects/idg-minimal/idg.py
@@ -2,7 +2,7 @@ import signac
 
 project = signac.init_project()
 
-for p in 0.1, 1.0, 10.0:
+for p in [0.1, 1.0, 10.0]:
     sp = {"p": p, "kT": 1.0, "N": 1000}
 
     with project.open_job(sp) as job:
