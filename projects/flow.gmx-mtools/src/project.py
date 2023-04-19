bdca33a6bcfeed15f76c24551c25bdff3306b250
Bradley Dice <bdice@bradleydice.com>
2023-04-18 19:18:55 -0500

    Update flow.gmx-mtools. Failing due to use of Berendsen thermostat/barostat, which raises fatal warnings.

--- projects/flow.gmx-mtools/src/project.py
+++ projects/flow.gmx-mtools/src/project.py
@@ -1,5 +1,4 @@
 """Define the project's workflow logic."""
-import flow
 import mbuild as mb
 import signac
 from flow import FlowProject
@@ -10,7 +9,7 @@ project_path = signac.get_project().path
 def _grompp_str(path, op_name, gro_name, sys_name):
     """Helper function, returns grompp command string for operation"""
     cmd = (
-        "gmx grompp -f {path}/src/util/mdp_files/{op}.mdp -c {gro}.gro "
+        "gmx grompp -f {path}/mdp_files/{op}.mdp -c {gro}.gro "
         "-p {sys}.top -o {op}.tpr"
     )
     return cmd.format(path=path, op=op_name, gro=gro_name, sys=sys_name)
@@ -23,7 +22,7 @@ def _mdrun_str(op_name):
 
 def gromacs_command(name, gro, sys):
     """Simplify GROMACS operations"""
-    return "cd {{job.ws}} ; {} && {}".format(
+    return "{} && {}".format(
         _grompp_str(project_path, name, gro, sys),
         _mdrun_str(name),
     )
@@ -53,40 +52,36 @@ def sampled(job):
     return job.isfile("sample.gro")
 
 
-@MyProject.operation
 @MyProject.post(initialized)
+@MyProject.operation(with_job=True)
 def initialize(job):
     """Initialize the simulation."""
-    with job:
-        alkane = mb.recipes.Alkane(job.statepoint()["C_n"])
-        n_alkane = 200
-        # A cleaner packing approach would involve pull #372
-        system_box = mb.Box([4, 4, 4])
-        system = mb.fill_box(compound=alkane, n_compounds=n_alkane, box=system_box)
-        system.save("init.gro", overwrite=True)
-        system.save("init.top", forcefield_name="oplsaa", overwrite=True)
+    alkane = mb.recipes.Alkane(job.statepoint()["C_n"])
+    n_alkane = 200
+    # A cleaner packing approach would involve pull #372
+    system_box = mb.Box([4, 4, 4])
+    system = mb.fill_box(compound=alkane, n_compounds=n_alkane, box=system_box)
+    system.save("init.gro", overwrite=True)
+    system.save("init.top", forcefield_name="oplsaa", overwrite=True)
 
 
-@MyProject.operation
 @MyProject.pre(initialized)
 @MyProject.post(minimized)
-@flow.cmd
+@MyProject.operation(cmd=True, with_job=True)
 def em(job):
     return gromacs_command(name="em", gro="init", sys="init")
 
 
-@MyProject.operation
 @MyProject.pre(minimized)
 @MyProject.post(equilibrated)
-@flow.cmd
+@MyProject.operation(cmd=True, with_job=True)
 def equil(job):
     return gromacs_command(name="equil", gro="em", sys="init")
 
 
-@MyProject.operation
 @MyProject.pre(equilibrated)
 @MyProject.post(sampled)
-@flow.cmd
+@MyProject.operation(cmd=True, with_job=True)
 def sample(job):
     return gromacs_command(name="sample", gro="equil", sys="init")
 
