66138f195ce911667fef20d6d54b3885c4b56f2b
Brandon Butler <butlerbr@umich.edu>
2023-10-11 15:11:06 -0400

    refactor: Move label.py and workflow.py to project.py

--- projects/flow.pytorch.HyperparameterOptimize/project.py
+++ projects/flow.pytorch.HyperparameterOptimize/project.py
@@ -1,18 +1,54 @@
 import numpy as np
 import signac
-from source import labels, workflow
-from source.workflow import Project, cpu_directives
+from flow import FlowProject
+
+
+class Project(FlowProject):
+    pass
+
+
+@Project.label
+def status_label(job):
+    return ", ".join(
+        [
+            f"{check_point}_completed"
+            for check_point in ("train", "evaluation")
+            if job.doc.get(check_point + "_done", False)
+        ]
+    )
+
+
+def check_train_complete(job):
+    return job.doc.get("train_done", False)
+
+
+def check_eval_complete(job):
+    return job.doc.get("evaluation_done", False)
+
+
+def gpu_directives(walltime: float = 0.5, n_gpu: int = 1):
+    return {"nranks": n_gpu, "ngpu": n_gpu, "walltime": walltime}
+
+
+def cpu_directives(walltime: float = 0.5, n_cpu: int = 1):
+    return {"nranks": n_cpu, "walltime": walltime}
+
+
+def store_success_to_doc(operation_name, job):
+    job.doc.update({f"{operation_name}_done": True})
+
+
+training_group = Project.make_group(name="trainings")
 
 PR = signac.get_project()
 TRAIN_WALLTIME = 1
 EVAL_WALLTIME = 0.5
 
 
-@workflow.training_group
+@training_group
 @Project.operation(directives=cpu_directives(walltime=TRAIN_WALLTIME))
-@Project.operation_hooks.on_success(workflow.store_success_to_doc)
-@Project.operation_hooks.on_exception(workflow.store_error_to_doc)
-@Project.post(labels.check_train_complete)
+@Project.operation_hooks.on_success(store_success_to_doc)
+@Project.post(check_train_complete)
 def train(job):
     """ """
     import torch
@@ -23,12 +59,11 @@ def train(job):
     vae.fit(job=job, train_loader=train_loader, val_loader=val_loader, device=device)
 
 
-@workflow.training_group
+@training_group
 @Project.operation(directives=cpu_directives(walltime=EVAL_WALLTIME))
 @Project.pre.after(train)
-@Project.operation_hooks.on_success(workflow.store_success_to_doc)
-@Project.operation_hooks.on_exception(workflow.store_error_to_doc)
-@Project.post(labels.check_eval_complete)
+@Project.operation_hooks.on_success(store_success_to_doc)
+@Project.post(check_eval_complete)
 def evaluation(job):
     import torch
     from source import vae
