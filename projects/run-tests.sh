6ebd462dc32ba6af5d7b70c89fd80c38d17a3ee6
Bradley Dice <bdice@bradleydice.com>
2023-11-04 15:36:21 -0500

    Clean up CI output.

--- projects/run-tests.sh
+++ projects/run-tests.sh
@@ -3,23 +3,19 @@
 set -euo pipefail
 
 for PROJECT in `ls -d */`; do
-    if [[ "$CI" ]]; then
-        echo "::group::Testing ${PROJECT}"
-    fi
-
     if [ -e "${PROJECT}/.skipci" ]; then
-        echo "Skipping tests for project ${PROJECT}."
-        if [[ "$CI" ]]; then
-            echo "::endgroup::"
-        fi
+        echo "Skipping ${PROJECT}"
         continue
     fi
+    if [[ "$CI" ]]; then
+        echo "::group::Testing ${PROJECT}"
+    else
+        echo "Testing ${PROJECT}"
+    fi
     REQUIREMENTS_FILE="${PROJECT}/requirements.txt"
-    echo "Run test for ${PROJECT}."
     if [ -e ${REQUIREMENTS_FILE} ]; then
         echo "Installing requirements:"
         cat ${REQUIREMENTS_FILE}
-
         mamba install --yes --file ${REQUIREMENTS_FILE} --quiet
     fi
     python flow-test.py ${PROJECT} -vv --timeout=600 $@
